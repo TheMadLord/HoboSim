8801971724fa5b99762c721e0c73f18990d5a64e
jefferey schlueter <jschlueter@ad.ssucet>
2019-01-26 18:38:08 -0500

    I added 3 more bits off art for the skills menu, moved both menus to opening on the lefthand side of the screen and forced only one menu to be open at a time. Added functionality and flavor text for the Building Skill tree

--- Assets/GUI_Scripts/GUI_LefthandMenu.cs
+++ Assets/GUI_Scripts/GUI_LefthandMenu.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 
 public class GUI_LefthandMenu : MonoBehaviour {
 
+    protected bool isMenuActive;
+    public GameObject[] relatedObjs;
     public GameObject menu1;
     public GameObject menu2;
-
+    
+    void Update(){
+        isMenuActive = false;
+        foreach (GameObject obj in relatedObjs)
+        {
+            if (obj.activeSelf)
+            {
+                isMenuActive = true;
+                break;
+            }
+        }
+    }
     public void ToggleMenu()
     {
-        menu1.SetActive(!menu1.activeSelf);
-        menu2.SetActive(!menu2.activeSelf);
+        if (!isMenuActive)
+        {
+            menu1.SetActive(!menu1.activeSelf);
+            menu2.SetActive(!menu2.activeSelf);
+        }
     }
 }
---
+++ Assets/GUI_Scripts/GUI_PlayerProfileButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUI_PlayerProfileButton : MonoBehaviour {
+
+    protected bool isMenuActive;
+    public GameObject menu;
+    public GameObject[] relatedObjs;
+
+    void Start(){
+    }
+
+    void Update(){
+        isMenuActive = false;
+        foreach (GameObject obj in relatedObjs)
+        {
+            if (obj.activeSelf)
+            {
+                isMenuActive = true;
+                break;
+            }
+        }
+    }
+
+    public void toggleMenu(){
+        if (!isMenuActive)
+        {
+            menu.SetActive(!menu.activeSelf);
+        }
+        
+    }
+}
