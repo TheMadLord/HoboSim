c08cd20f9ae4190c17877496138c0ca8f7b61ab8
Mallboy <47044742+Mallboy@users.noreply.github.com>
2019-01-25 22:02:51 -0500

    vdgbjkkafbiks
    Enemy Stuff

---
+++ Assets/WaveSpawner.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public  Transform enemyGameObject;
+
+    public float timeBetween = 20f;
+    private float countTime;
+
+    private int waveNum = 0;
+
+    public int numEnemies = 10;
+
+    private bool waveEnded;
+
+    public void Start() {
+        countTime = timeBetween;
+        waveEnded = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(countTime <= 0f)
+        {
+            waveEnded = false;
+            StartCoroutine(SpawnWave());
+            countTime = timeBetween;
+        }
+
+        if (waveEnded)
+        {
+            countTime -= Time.deltaTime;
+        }
+	}
+
+    IEnumerator SpawnWave()
+    {
+        waveNum++;
+
+        for (int i = 0; i < numEnemies; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+        waveEnded = true;
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyGameObject, transform.position, transform.rotation);
+    }
+}
