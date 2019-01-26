5365f6350d4a8eb4cd35115e8bd43951dfe20766
Mallboy <47044742+Mallboy@users.noreply.github.com>
2019-01-26 09:56:35 -0500

    Create BoxLiving.cs

---
+++ Assets/BoxLiving.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxLiving : MonoBehaviour {
+
+    private int mHealth;
+	// Use this for initialization
+	void Start () {
+        mHealth = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void damage(int amt)
+    {
+        mHealth -= amt;
+        if(mHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
