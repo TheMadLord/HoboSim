b102672eec07ed5c7448bb0e9181dd6760607c26
jefferey schlueter <jschlueter@ad.ssucet>
2019-01-26 04:21:06 -0500

    I made the trash shop, trash counter, and half a health bar

---
+++ Assets/GUI_Scripts/Trash_Counter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Trash_Counter : MonoBehaviour {
+
+    protected int trash = 0;
+    public Text trashText;
+
+	void Start () {
+	}
+
+	void Update () {
+        trashText.text = "TRASH : " + trash;	
+	}
+   
+}
