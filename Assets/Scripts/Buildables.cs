e0060bb8685948db184004c88bde0f76552a051d
ryan tollefson <rtollefson@ad.ssucet>
2019-01-26 11:07:14 -0500

    a lilltle more work

--- Assets/Scripts/Buildables.cs
+++ Assets/Scripts/Buildables.cs
@@ -7,4 +7,16 @@ public class Buildables : MonoBehaviour
 {
     public int cost;
     public Tile[] buildableOn;
+    public float heath;
+
+    public void Damage(float amt) {
+        heath -= amt;
+        if (heath <= 0)
+        {
+            destroy();
+        }
+    }
+    public void destroy() {
+        GameObject.Destroy(gameObject);
+    }
 }
