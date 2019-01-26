6d824394cc9fe02a92ab18ac4cc9d6522dd6d4c7
ryan tollefson <rtollefson@ad.ssucet>
2019-01-26 03:56:54 -0500

    Digging and the begaining of Building

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public  Transform enemyGameObject;
+
+    public float timeBetween = 20f;
+    private float countTime;
+
+    private int waveNum = 0;
+
+    public int numEnemies = 10;
+
+    private bool waveEnded;
+
+    public void Start() {
+        countTime = timeBetween;
+        waveEnded = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(countTime <= 0f)
+        {
+            waveEnded = false;
+            StartCoroutine(SpawnWave());
+            countTime = timeBetween;
+        }
+
+        if (waveEnded)
+        {
+            countTime -= Time.deltaTime;
+        }
+	}
+
+    IEnumerator SpawnWave()
+    {
+        waveNum++;
+
+        for (int i = 0; i < numEnemies; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+        waveEnded = true;
+    }
+
+    void SpawnEnemy()
+    {
+        GameObject  go = Instantiate(enemyGameObject, transform.position, transform.rotation).gameObject;
+        //go.GetComponent<NavMeshAgent>().SetDestination(new Vector3(10, 1, 10));
+    }
+}
