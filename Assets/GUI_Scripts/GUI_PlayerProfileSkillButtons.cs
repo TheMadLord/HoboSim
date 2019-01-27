f8c04366338a0eb4e9bbd1a1628a6e5aec9e1c13
jefferey schlueter <jschlueter@ad.ssucet>
2019-01-27 15:01:15 -0500

    Skills galore

--- Assets/GUI_Scripts/GUI_PlayerProfileSkillButtons.cs
+++ Assets/GUI_Scripts/GUI_PlayerProfileSkillButtons.cs
@@ -10,63 +10,23 @@ public class GUI_PlayerProfileSkillButtons : MonoBehaviour {
     public Image imageSource;
     public GUI_PlayerProfileReadMoretab target;
     public int ID;
-    public bool isSkillActive;
-    protected bool isSkillUsed;
-    protected int Cost;
+    //public bool isSkillActive;
+    public bool isSkillUsed;
+    public int Cost;
 
     void Start() {
         Button b = GetComponent<Button>();
         imageSource = b.image;
+        MECH_SkillPoints.Skills[ID] = this;
 
         b.onClick.AddListener(() =>
         {
             target.SetText(textSource, imageSource, ID);
         });
-
-        b.onClick.AddListener(() =>
-        {
-            if (!isSkillUsed)
-            {
-                switch (ID)
-                {
-                    case 300:
-                        if (MECH_SkillPoints.SkillPoints - Cost > 0)
-                        {
-                            MECH_SkillPoints.SkillPoints -= Cost;
-                            MECH_SkillPoints.trashCarryWeight++;
-                            isSkillUsed = true;
-                        }
-                        break;
-                    case 301:
-                        if (MECH_SkillPoints.SkillPoints - Cost > 0)
-                        {
-                            MECH_SkillPoints.SkillPoints -= Cost;
-                            MECH_SkillPoints.trashCarryWeight++;
-                            isSkillUsed = true;
-                        }
-                        break;
-                    case 302:
-                        if (MECH_SkillPoints.SkillPoints - Cost > 0)
-                        {
-                            MECH_SkillPoints.SkillPoints -= Cost;
-                            MECH_SkillPoints.trashCarryWeight++;
-                            isSkillUsed = true;
-                        }
-                        break;
-                    case 303:
-                        if (MECH_SkillPoints.SkillPoints - Cost > 0)
-                        {
-                            MECH_SkillPoints.SkillPoints -= Cost;
-                            MECH_SkillPoints.trashCarryWeight++;
-                            isSkillUsed = true;
-                        };
-                        break;
-                }
-            }
-        });
+        
         imageSource.color = Color.black;
         Cost = (ID % 100) * 5;
-        isSkillActive = false;
+        //isSkillActive = false;
         isSkillUsed = false;
     }
 
